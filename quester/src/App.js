0d16f805d70c713b0d5d8457e3926c9ce0081f0b
Tahmidur_Rabb <tr1476@nyu.edu>
2023-01-15 18:32:05 -0500

    Creation of react app

---
+++ quester/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Welcome to Quester! </h1>
+        <h3> To get started enter the tasks you have right now and proceed to the next page </h3>
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
